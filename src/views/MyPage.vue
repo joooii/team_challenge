6eacd7e2120955537c05400bae7817684e0c3c9b
Juhee Kim <3098k@naver.com>
2022-08-15 01:11:52 +0900

    8.15 boder-right 추가

--- src/views/MyPage.vue
+++ src/views/MyPage.vue
@@ -4,25 +4,27 @@
   </div>
   <div class="container">
     <div class="top">
-      <!-- <span class="account_section"> -->
         <div class="top-left">
           <div class="name">
-            <p>이름: 신준성</p>
+            <div class="name_box">
+              <b>NAME</b>
+            </div>
           </div>
           <br>
+
           <div class="id">
-            <p>아이디: 990114js</p>
+            <div class="id_box">
+              <b>ID</b>
+            </div>
           </div>
         </div>
-      <!-- </span> -->
-    <!-- <span class="remind_section"> -->
+
       <div class="remind_list">
         <img alt="Vue logo" src="../assets/logo.png">
       </div>
-    <!-- </span> -->
     </div>
+
     <div class="bottom">
-      <!-- <span class="challenging_section"> -->
       <div class="challenging">
         <div class="ongoing_list">
           <b>진행중인 챌린지</b>
@@ -30,13 +32,8 @@
         <ul>
           <li v-for="(ongoingChallenge, i) in ongoingChallenge" :key="i">{{ ongoingChallenge }}</li>
         </ul>
-        <!-- 진행중인 챌린지<br>
-        부트캠프(22/07/01~22/08/21)<br>
-        운동(22/01/01~22/12/31)<br>
-        기사 시험(22/09/01~22/12/31) -->
       </div>
-      <!-- </span> -->
-      <!-- <span class="finish_section"> -->
+
         <div class="finish_challenge">
           <div class="finish_list">
             <b>완료한 챌린지</b>
@@ -44,13 +41,7 @@
           <ul>
             <li v-for="(completedChallenge, i) in completedChallenge" :key="i">{{ completedChallenge }}</li>
           </ul>
-
-        <!-- 완료한 챌린지<br>
-        1학기 중간고사(22/04/01~22/04/26)<br>
-        1학기 기말고사(22/06/01~22/06/24)<br>
-        이른 기상(22/01/01~22/06/31) -->
-        </div>
-      <!-- </span>/ -->
+      </div>
     </div>
   </div>
 </template>
@@ -74,28 +65,18 @@ h1 {
   margin-bottom: 60px;
 }
 
+b{
+  font-size:17px;
+  /* color: rgb(0,42,134); */
+}
+
 .container{
-  /* padding: 5%; */
+  padding: 5%;
   flex-direction: column;
   padding-right: 10%;
   padding-left: 10%;
 }
-.account_section{
-  float: left;
-  margin-left: 10%;
-}
-.remind_section{
-  float: right;
-  margin-right: 10%;
-}
-.challenging_section{
-  float: left;
-  margin-left: 10%;
-}
-.finish_section{
-  float: right;
-  margin-right: 10%;
-}
+
 .top{
   display: flex;
   /* height: 250px; */
@@ -105,6 +86,7 @@ h1 {
   display: flex;
   margin-left:10%;
 }
+
 nav a.router-link-exact-active {
   color: #42b983;
 }
@@ -118,6 +100,17 @@ nav a.router-link-exact-active {
   width: 400px;
   height: 50px;
   flex-direction: column;
+}
+
+.name_box, .id_box {
+  position: absolute;
+  width: inherit;
+  max-width: 80px;
+  height: 36px;
+  padding-top: 15px;
+  border-right: 1px solid rgb(174, 174, 174);
+  font-size: 17px;
+  /* color: rgb(0,42,134); */
 }
 
 .id {
